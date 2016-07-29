c519cf90ce3e7478f2d7fe1b5dbc8611d0b9248a
Zack Repass <jzrepass@gmail.com>
2016-07-29 15:28:07 -0500

    done

--- quiz.js
+++ quiz.js
@@ -1,25 +1,31 @@
-var tree = {};
+
 var growButton = document.getElementById('grow_button');
-growButton.addEventListener("click", runner);
+growButton.addEventListener("click", function () {
+  var tree = {};
+  tree.height = treeHeight.value;
+  tree.branch = branchChar.value;
+  runner(tree)
+});
 var showTree = document.getElementById('show_tree');
 var branchChar = document.getElementById('branch_character');
 var treeHeight = document.getElementById('tree_height');
-branchChar.addEventListener("keyup", enterF);
+document.body.addEventListener("keyup", enterF);
 
 function enterF (e) {
   if (e.key === 'Enter') {
+    var tree = {};
     tree.height = treeHeight.value;
     tree.branch = branchChar.value;
     runner (tree);
-    console.log(tree)
-}
+    console.log(tree);
+  }
 }
 
 function runner (tree) {
+  var spaces = " ";
 
-var spaces = " ";
-  for (i = 1; i <= tree.height; i++) {
-  console.log(spaces.repeat(tree.height - i) + (tree.branch.repeat((2*i) - 1)));
-  //showTree.innerHTML = "spaces.repeat(treeHeight - i) + (branchChar.repeat((2*i) - 1))";
+  for (var i = 1; i <= tree.height; i++) {
+    console.log(spaces.repeat(tree.height - i) + (tree.branch.repeat((2*i) - 1)));
+    //showTree.innerHTML = "spaces.repeat(treeHeight - i) + (branchChar.repeat((2*i) - 1))";
   }
 }
